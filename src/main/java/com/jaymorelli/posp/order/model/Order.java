041c2c1e75c78a30123975777a7a9141dd68dc5f
Ramos Morelli <jean.morelli@atos.net>
2022-02-23 10:21:01 +0000

    added second jenkins file for sonar - testing

---
+++ src/main/java/com/jaymorelli/posp/order/model/Order.java
@@ -0,0 +1,13 @@
+package com.jaymorelli.posp.order.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    private String id;
+}
